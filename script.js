c481bfa0047ae4633349012460b572133eb668dc
essram <esarama22@gmail.com>
2024-06-26 14:36:48 +0700

    Update

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var copy = document.querySelector(".logos-slide").cloneNode(true);
-      document.querySelector(".logos").appendChild(copy);
+// JavaScript code for the dark mode toggle functionality
+const toggleButton = document.querySelector(".dark-mode-toggle");
+const bodyElement = document.body;
+const navbar = document.querySelector(".navbar");
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark-mode");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
